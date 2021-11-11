5ffcce64d9b6db12798222e6be9c388e8fe795b4
Shane Frost <shane.frost@cloudreach.com>
2021-11-11 17:31:46 +0000

    third bit

--- main.py
+++ main.py
@@ -2,6 +2,12 @@ from flask import Flask, request, Response
 
 app = Flask(__name__)
 
+movie_db = {
+    "1" : { "name" : "Stargate", "Release Date" : "1994"},
+    "2" : { "name" : "Sunshine", "Release_Date" : "2007"},
+    "3" : { "name" : "The Holiday", "Release Date" : "2006" }
+}
+
 @app.route("/")
 def hello():
     return "Hello World"
@@ -9,6 +15,10 @@ def hello():
 @app.route("/hello")
 def world():
     return "<h1> Hello you !!! </h1>"
+
+@app.route("/movies")
+def Movies():
+    return movie_db
 
 if __name__ == "__main__":
     app.run(host='127.0.0.1')
